90b08e2c3b73fedbb52c658cb8156205f3603bfd
lishengping2010@163.com <lishengping2010@163.com>
2024-07-10 14:32:47 +0800

    dataset type lead to save checkpoint item name error

--- MaxText/train.py
+++ MaxText/train.py
@@ -171,7 +171,7 @@ def save_checkpoint(checkpoint_manager, step, state, dataset_type="c4", data_ite
             iter=grain.PyGrainCheckpointSave(data_iterator.local_iterator),
         ),
     )
-  elif dataset_type == "pile":
+  elif dataset_type in ["pile", 'novel']: # lsp
     return checkpoint_manager.save(step, {'state': state})
   else:
     return checkpoint_manager.save(

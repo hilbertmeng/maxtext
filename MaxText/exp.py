f4acc22e62e0e875cf06c57338a0d69ac0eab11a
lishengping2010@163.com <lishengping2010@163.com>
2025-04-11 09:47:29 +0800

    add mistral medium moe class

--- MaxText/exp.py
+++ MaxText/exp.py
@@ -229,7 +229,10 @@ class Llama2MediumOLMoe(CommonMoe, Llama2Medium):
     base_mlp_dim = 352
     qk_norm = False
 
-class Llama2MediumMistralMoe(CommonMoe, Llama2Medium):
+class Llama2MediumMistralMoe(Llama2MediumOpenMoe):
+    moe_type = 'mistral'
+
+class Llama2LargeMistralMoe(Llama2LargeOpenMoe):
     moe_type = 'mistral'
 
 class MuddLlama2MeduimMistralMoe(MuddLlama2MediumOpenMoe):

32407243ab35a14eab18ba1314f19ce564d7bb48
lishengping2010@163.com <lishengping2010@163.com>
2025-05-14 12:22:00 +0800

    unnormal data skip

--- MaxText/train.py
+++ MaxText/train.py
@@ -1053,6 +1053,9 @@ def train_loop(config, state=None):
       with jax.profiler.StepTraceAnnotation("train", step_num=step):
         record_goodput(recorder, config, recorder.record_data_loading_start_time if recorder else None)
         example_batch = load_next_batch(data_iterator, example_batch, config)
+        if 105178 < step < 105188:
+          print(f'Skip step: {step}')
+          continue
         record_goodput(recorder, config, recorder.record_data_loading_end_time if recorder else None)
         check_example_batch(config, example_batch=example_batch)
         # pylint: disable=not-callable

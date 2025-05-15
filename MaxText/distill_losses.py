184ca12a8a0d2bf1ca6a0837f5a77c35cacfd4de
lishengping2010@163.com <lishengping2010@163.com>
2025-05-15 19:21:04 +0800

    distill model code opt2

--- MaxText/distill_losses.py
+++ MaxText/distill_losses.py
@@ -12,8 +12,8 @@ def distillation_loss(student_logits, teacher_logits, mask=None, temperature=2.0
 
 
 def forward_kld_topk(logits, teacher_logits, k: int):
-    logits = jnp.asarray(logits, dtype=jnp.float32)
-    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.float32)
+    logits = jnp.asarray(logits, dtype=jnp.bfloat16)
+    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.bfloat16)
 
     if logits.shape != teacher_logits.shape:
         raise ValueError(
@@ -28,21 +28,21 @@ def forward_kld_topk(logits, teacher_logits, k: int):
     teacher_probs = jax.nn.softmax(teacher_logits, axis=-1)
     student_logprobs = jax.nn.log_softmax(logits, axis=-1)
     _, top_k_indices = jax.lax.top_k(teacher_logits, k=k_val)
-    top_k_mask = jnp.sum(jax.nn.one_hot(top_k_indices, num_classes, dtype=jnp.float32), axis=-2)
+    top_k_mask = jnp.sum(jax.nn.one_hot(top_k_indices, num_classes, dtype=jnp.bfloat16), axis=-2)
     effective_teacher_probs = teacher_probs * top_k_mask
-    safe_student_logprobs = jnp.where(
-        effective_teacher_probs == 0.0, 
-        0.0,                           
-        student_logprobs              
-    )
-    prod_probs = safe_student_logprobs * effective_teacher_probs
+    # safe_student_logprobs = jnp.where(
+    #     effective_teacher_probs == 0.0, 
+    #     0.0,                           
+    #     student_logprobs              
+    # )
+    prod_probs = student_logprobs * effective_teacher_probs
     distill_xent = -jnp.sum(prod_probs, axis=-1)
     return distill_xent
 
 
 def reverse_kld_topk(logits, teacher_logits, k: int):
-    logits = jnp.asarray(logits, dtype=jnp.float32)
-    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.float32)
+    logits = jnp.asarray(logits, dtype=jnp.bfloat16)
+    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.bfloat16)
 
     if logits.shape != teacher_logits.shape:
         raise ValueError(
@@ -59,22 +59,21 @@ def reverse_kld_topk(logits, teacher_logits, k: int):
     student_probs = jax.nn.softmax(logits, axis=-1)
 
     _, top_k_indices = jax.lax.top_k(teacher_logits, k=k_val)
-    top_k_mask = jnp.sum(jax.nn.one_hot(top_k_indices, num_classes, dtype=jnp.float32), axis=-2)
+    top_k_mask = jnp.sum(jax.nn.one_hot(top_k_indices, num_classes, dtype=jnp.bfloat16), axis=-2)
     effective_teacher_logprobs = teacher_logprobs * top_k_mask
-
-    safe_student_probs = jnp.where(
-        effective_teacher_logprobs == 0.0, 
-        0.0,                           
-        student_probs              
-    )
-    prod_probs = safe_student_probs * effective_teacher_logprobs
+    # safe_student_probs = jnp.where(
+    #     effective_teacher_logprobs == 0.0, 
+    #     0.0,                           
+    #     student_probs              
+    # )
+    prod_probs = student_probs * effective_teacher_logprobs
     distill_xent = -jnp.sum(prod_probs, axis=-1)
     return distill_xent
 
 
 def bi_kld(logits, teacher_logits):
-    logits = jnp.asarray(logits, dtype=jnp.float32)
-    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.float32)
+    logits = jnp.asarray(logits, dtype=jnp.bfloat16)
+    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.bfloat16)
     inf_mask = jnp.isinf(logits) | jnp.isinf(teacher_logits)
     teacher_probs = jax.nn.softmax(teacher_logits, axis=-1)
     student_logprobs = jax.nn.log_softmax(logits, axis=-1)
@@ -84,8 +83,8 @@ def bi_kld(logits, teacher_logits):
             
 
 def skewed_forward_kl(logits, teacher_logits, lam=0.1):
-    logits = jnp.asarray(logits, dtype=jnp.float32)
-    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.float32)
+    logits = jnp.asarray(logits, dtype=jnp.bfloat16)
+    teacher_logits = jnp.asarray(teacher_logits, dtype=jnp.bfloat16)
     teacher_probs = jax.nn.softmax(teacher_logits, axis=-1)
     student_probs = jax.nn.softmax(logits, axis=-1)
     mixed_probs = lam * teacher_probs + (1-lam) * student_probs
@@ -97,7 +96,7 @@ def skewed_forward_kl(logits, teacher_logits, lam=0.1):
 
 
 def skewed_reverse_kl(logits, teacher_logits, lam=0.1):
-    logits = jnp.asarray(logits, dtype=jnp.float32)
+    logits = jnp.asarray(logits, dtype=jnp.bfloat16)
     teacher_logits = jnp.asarray(teacher_logits)
     teacher_probs = jax.nn.softmax(teacher_logits, axis=-1)
     student_probs = jax.nn.softmax(logits, axis=-1)

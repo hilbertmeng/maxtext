0914707e33a4374904627b62fc73f320c6fdc7fd
lishengping2010@163.com <lishengping2010@163.com>
2025-05-17 10:13:02 +0800

    xm mini stage 4

--- MaxText/train.py
+++ MaxText/train.py
@@ -1053,7 +1053,8 @@ def train_loop(config, state=None):
       with jax.profiler.StepTraceAnnotation("train", step_num=step):
         record_goodput(recorder, config, recorder.record_data_loading_start_time if recorder else None)
         example_batch = load_next_batch(data_iterator, example_batch, config)
-        if 105178 < step < 105188:
+        # if 105178 < step < 105188:
+        if 150416 < step < 150426:
           print(f'Skip step: {step}')
           continue
         record_goodput(recorder, config, recorder.record_data_loading_end_time if recorder else None)

7ae56c8a2ae4b9338a4991115694717aa9f361b5
lishengping2010@163.com <lishengping2010@163.com>
2025-12-14 09:37:24 +0800

    update exp

--- MaxText/exp.py
+++ MaxText/exp.py
@@ -658,13 +658,15 @@ class MuonDEDcMuddMTP1KVshiftV4p5XLData400BGH128(MuonDEDcMuddMTP1KVshiftV4p5XLDa
     eval_steps = 156
     eval_interval = 5000
     keep_period = 2000
-    train_shuffle_buffer_size = 100000
-    iter_file_nums = 100
+    train_shuffle_buffer_size = 200000
+    iter_file_nums = 500
     dynamic_mlp_dim = False
     deep_embed_type = '4xmlp'
     deep_embed_init = 'outside'
     loss_chunk_size = 4096
     learning_rate_schedule_steps = 190735
+    zero_loss = True # olmo2-1B zero means '!'
+    eval_split = 'validation'
     
 class MuonMuddDEDcMuddMTP1KVshiftV4p5XLData400BGH128T20A5(MuonDEDcMuddMTP1KVshiftV4p5XLData400BGH128):
     mudd_emb_dilation = 4

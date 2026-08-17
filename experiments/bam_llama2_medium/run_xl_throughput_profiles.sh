bf5996269356cbdb841623e2229200f22113943e
xiaoda99 <xiaoda99@gmail.com>
2026-08-16 23:04:12 -0400

    Use absltest for XL TPU validation

--- experiments/bam_llama2_medium/run_xl_throughput_profiles.sh
+++ experiments/bam_llama2_medium/run_xl_throughput_profiles.sh
@@ -41,8 +41,9 @@ gcloud compute tpus tpu-vm ssh --internal-ip "$TPU" --zone="$ZONE" \
 gcloud compute tpus tpu-vm ssh --internal-ip "$TPU" --zone="$ZONE" \
   --project="$PROJECT" --worker=0 \
   --command="cd '$REMOTE_REPO' && PYTHONPATH=MaxText \
-  /home/lishengping/miniconda3/bin/python -m pytest -q \
-  MaxText/tests/bam_attention_test.py -k bam_read_head_mapping"
+  /home/lishengping/miniconda3/bin/python \
+  MaxText/tests/bam_attention_test.py \
+  --test_filter='*bam_read_head_mapping*'"
 
 mkdir -p "$LOG_ROOT"
 for exp_class in "$@"; do

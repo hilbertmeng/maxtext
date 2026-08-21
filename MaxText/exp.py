97c10976f8f48a9e00c3fa138e758efe67ae153b
xiaoda99 <xiaoda99@gmail.com>
2026-08-20 21:49:19 -0400

    Record XL 32x64 launch throughput

--- MaxText/exp.py
+++ MaxText/exp.py
@@ -1985,7 +1985,7 @@ class BamLlama2XLHead32x64V2C256(
     BamLlama2XLHead32x64V2C256T2048Profile
 ):
     """50k-step XL 32x64 BAM head-shape scalability run on v5p-32."""
-    # code_commit: 312a8ce; running.
+    # code_commit: 6ed397a; EW4b ~0.477 steps/s; running.
     model_name = 'BamLlama2XLHead32x64V2C256'
     profiler = ''
     profile_periodically_period = -1
@@ -2004,7 +2004,7 @@ class BamMHALlama2XLHead32x64C256(
     BamMHALlama2XLHead32x64C256T2048Profile
 ):
     """Matched 50k-step BamAttention MHA control for XL 32x64."""
-    # code_commit: 312a8ce; running.
+    # code_commit: 6ed397a; EW4b ~0.646 steps/s; running.
     model_name = 'BamMHALlama2XLHead32x64C256'
     profiler = ''
     profile_periodically_period = -1
